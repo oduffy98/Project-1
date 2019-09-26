71b0dfdab5147f6e0edfedde2171bad767740dab
ksmith33 <Ladasha1>
2019-09-26 02:09:44 -0400

    Done with Enter, Quit, and Two sorts

---
+++ src/com/company/Main.java
@@ -0,0 +1,119 @@
+package com.company;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Double;
+import java.lang.Integer;
+import java.lang.Boolean;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Inventory parts = new Inventory();
+        String userInput = "blah";
+        File inFile = new File("WareHouseDB.txt");
+        Scanner sc = new Scanner(inFile);
+        while (sc.hasNextLine()) {
+            String newEntry = sc.next();
+            String[] newObj = newEntry.split(",");
+            String n = newObj[0];
+            int id = Integer.parseInt(newObj[1]);
+            double p = Double.parseDouble(newObj[2]);
+            double sp = Double.parseDouble(newObj[3]);
+            boolean s = Boolean.parseBoolean(newObj[4]);
+            int q = Integer.parseInt(newObj[5]);
+            BikePart bpName = new BikePart(n, id, p, sp, s, q);
+            parts.addPart(bpName);
+
+        }
+        sc.close();
+
+        Scanner input = new Scanner(System.in);
+        while ((!userInput.equals("Quit")) && ((!userInput.equals("quit"))) && (!userInput.equals("QUIT"))) {
+            System.out.println("Please select your option from the following menu:");
+            System.out.println("Read: Read an inventory delivery file");
+            System.out.println("Enter: Enter a part");
+            System.out.println("Display: Display a part");
+            System.out.println("SortName: Sort parts by part name");
+            System.out.println("SortNumber: Sort parts by part number");
+            System.out.println("Quit");
+            System.out.println("Enter your choice: ");
+
+            userInput = input.next();
+
+            if ((userInput.equals("Read")) || (userInput.equals("read"))) {
+                System.out.println("Please enter the name of the delivery file: ");
+                File file = new File(input.next());
+                Scanner fileReader = new Scanner(file);
+                while (fileReader.hasNextLine()) {
+                    String newEntry = fileReader.next();
+                    String[] newObj = newEntry.split(",");
+                    String n = newObj[0];
+                    int id = Integer.parseInt(newObj[1]);
+                    double p = Double.parseDouble(newObj[2]);
+                    double sp = Double.parseDouble(newObj[3]);
+                    boolean s = Boolean.parseBoolean(newObj[4]);
+                    int q = Integer.parseInt(newObj[5]);
+                    BikePart bpName = new BikePart(n, id, p, sp, s, q);
+                    String add = parts.checkPart(bpName);
+                    if (!add.equals("true")) {
+                        parts.addPart(bpName);
+                    }
+                }
+                fileReader.close();
+
+            } else if ((userInput.equals("Enter")) || (userInput.equals("ENTER"))) {
+                System.out.println("Please enter the part name, part number, price, sales price, and if the item is on sale, and the quantity");
+                input.useDelimiter(",|\r|\n");
+                String partName = input.next();
+                int partNumber = input.nextInt();
+                double price = input.nextDouble();
+                double salesPrice = input.nextDouble();
+                boolean onSale = input.nextBoolean();
+                int quantity = input.nextInt();
+                BikePart bP = new BikePart(partName, partNumber, price, salesPrice, onSale, quantity);
+                String add = parts.checkPart(bP);
+                if (!add.equals("true")) {
+                    parts.addPart(bP);
+                }
+                parts.print();
+                System.out.println();
+            } else if ((userInput.equals("SortName") || userInput.equals("sortname"))) {
+                Comparator<BikePart> compareByName = new Comparator<BikePart>() {
+                    @Override
+                    public int compare(BikePart o1, BikePart o2) {
+                        return o1.getPartName().compareTo(o2.getPartName());
+                    }
+                };
+                ArrayList<BikePart> sortList = parts.getInventory();
+                Collections.sort(sortList, compareByName);
+                System.out.print(sortList);
+            } else if ((userInput.equals("SortNumber")) || (userInput.equals("sortnumber") || (userInput.equals("sortNumber")))) {
+                Comparator<BikePart> compareByNumber = new Comparator<BikePart>() {
+                    @Override
+                    public int compare(BikePart o1, BikePart o2) {
+                        return o1.getPartNum().compareTo(o2.getPartNum());
+                    }
+                };
+                ArrayList<BikePart> sortList = parts.getInventory();
+                Collections.sort(sortList, compareByNumber);
+                System.out.print(sortList);
+            }
+
+        }
+        FileWriter fileWriter = new FileWriter("WareHouseDB.txt", false);
+        PrintWriter writer = new PrintWriter(fileWriter);
+        int counter = 0;
+        ArrayList<BikePart> partsToSave = parts.getInventory();
+        while(counter < partsToSave.size()){
+            writer.println(partsToSave.get(counter).toString());
+            ++counter;
+        }
+        writer.close();
+
+    }
+}
+
